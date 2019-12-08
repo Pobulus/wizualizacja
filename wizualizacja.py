f2c282fc5ff268f8da466084ca26d3ed8a8f480b
Pobulus <32367046+Pobulus@users.noreply.github.com>
2019-12-08 15:25:52 +0100

    Update wizualizacja.py

--- wizualizacja.py
+++ wizualizacja.py
@@ -1,11 +1,10 @@
 from tkinter import *
-import math
-import sys
+import math, sys
 root = Tk()
 root.title("Wizualizacja ruhu")
-width = 1000
-height = 500
-radius = 42
+width = 640
+height = 480
+radius = 19
 mp = 0.002#meters per pixel = 0.002, centimeter per pixel = 0.2
 group = LabelFrame(root, text="ruh", padx=5, pady=5)
 tlabel = Label(group)
@@ -38,13 +37,13 @@ def updatePoints(x):
         y1 = float(point[2])
         v1 = math.sqrt(math.pow(x1-float(ppoint[1]),2)+math.pow(y1-float(ppoint[2]),2))*mp/t-float(ppoint[0])
     else:
-        y1 = 250.0
+        y1 = 240.0
         v1 = (x1-float(ppoint[1]))*mp/(t-float(ppoint[0]))
     x1label.config(text = "Krążek 1 x="+str(x1))
     y1label.config(text = "Krążek 1 y="+str(y1))
     v1label.config(text = "Krążek 1 V="+str(v1))
     
-    c.coords("circ1",0,0,radius,radius)
+    c.coords("circ1",0,0,radius*2,radius*2)
     c.move("circ1", x1, y1)
     c.itemconfig("circ1", fill="yellow")
 
@@ -55,7 +54,7 @@ def updatePoints(x):
             y2 = float(point[5])
             v2 = math.sqrt(math.pow(x2-float(ppoint[4]),2)+math.pow(y2-float(ppoint[5]),2))*mp/t-float(ppoint[3])
         else:
-            y2 = 250.0
+            y2 = 240.0
             v2 = (x2-float(ppoint[4]))*mp/(t-float(ppoint[0]))
             
         x2label.config(text = "Krążek 2 x="+str(x2))
@@ -63,14 +62,10 @@ def updatePoints(x):
         
         v2label.config(text = "Krążek 2 V="+str(v2))
         vsumlabel.config(text="Suma prędkości: "+str(v1+v2))
-        c.coords("circ2",0,0,radius,radius)
+        c.coords("circ2",0,0,radius*2,radius*2)
         c.move("circ2", x2, y2)
         c.itemconfig("circ2", fill="red")
-
-if sys.argv[1][0] == "/":
-    filepath = sys.argv[1] 
-else:
-    filepath = sys.argv[1] #"./"+
+filepath = sys.argv[1] #"./"+
 points = []
 with open(filepath) as f:
     line = f.readline()
